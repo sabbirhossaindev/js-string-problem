e6ccb6585bdd2fdbae1865c54b4fbc596ca2244b
sabbir hossain <mdsabbir477470@gmail.com>
2022-08-07 13:05:28 +0600

    all done

---
+++ math.js
@@ -0,0 +1,18 @@
+/*--------------------------
+    pow(), 
+---------------------------*/
+
+const result = Math.pow(3, 8);
+console.log(result);
+
+const num1 = 25;
+const num2 = 45;
+const gap = num1 - num2;
+console.log(gap);
+
+if (gap < 5) {
+    console.log('you are my friend');
+}
+else {
+    console.log('you are not a friend');
+}
