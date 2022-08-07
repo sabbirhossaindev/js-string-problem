935a535213acb1ab26f4398489dc20268b486d0f
sabbir hossain <mdsabbir477470@gmail.com>
2022-08-07 14:18:47 +0600

    math done

--- math.js
+++ math.js
@@ -1,5 +1,5 @@
 /*--------------------------
-    pow(), 
+    Math.pow(), Math.abs(), Math.round(), Math.ceil(), Math.floor(), Math.random(),
 ---------------------------*/
 
 const result = Math.pow(3, 8);
@@ -7,7 +7,7 @@ console.log(result);
 
 const num1 = 25;
 const num2 = 45;
-const gap = num1 - num2;
+const gap = Math.abs(num1 - num2);
 console.log(gap);
 
 if (gap < 5) {
@@ -16,3 +16,28 @@ if (gap < 5) {
 else {
     console.log('you are not a friend');
 }
+
+// round();
+var number = 5.49999;
+var results = Math.round(number); // output = 5
+// console.log(results);
+
+// round();
+var number = 5.5;
+var results = Math.round(number); // output = 6
+// console.log(results);
+
+// ceil();
+var number = 5.000001;
+var results = Math.ceil(number); // output = 6
+// console.log(results);
+
+// floor();
+var number = 5.49999;
+var results = Math.floor(number); // output = 6
+// console.log(results);
+
+// random();
+var numbers = Math.random() * 100;
+var results = numbers;
+console.log(results);
